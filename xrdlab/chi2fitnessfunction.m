0dace18f713bd387f93373017e216584e13796ee
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2013-07-09 11:22:53 +0300

    Add new fitness functions to xrdlab

--- xrdlab/chi2fitnessfunction.m
+++ xrdlab/chi2fitnessfunction.m
@@ -5,9 +5,6 @@ function E = chi2fitnessfunction(simul,meas,g)
 
      err = zeros(size(meas));
 
-     %simul = simul * g.timeperstep;
-     %meas = meas * g.timeperstep;
-
      err = err + ((meas-simul).^2./meas);
      E = (sum(err)/length(meas))^(1/2);
 end
