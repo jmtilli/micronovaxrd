5d5bade03c2c5dfcd3c46ca7052bafd3844c2278
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2017-07-09 15:18:29 +0300

    Better XML detection

--- javasrc/fi/micronova/tkk/xray/measimport/PANImport.java
+++ javasrc/fi/micronova/tkk/xray/measimport/PANImport.java
@@ -342,6 +342,28 @@ public class PANImport {
         }
         return new Data(arrays);
     }
+    private static void readWhiteSpace(BufferedInputStream bs) throws IOException
+    {
+        byte[] array = new byte[1024];
+        int j = 0;
+outer:
+        while (true)
+        {
+            bs.mark(2048);
+            bs.read(array, 0, 1024);
+            for (int i = 0; i < 1024; i++)
+            {
+                if (array[i] != '\t' && array[i] != '\n' &&
+                    array[i] != '\r' && array[i] != ' ')
+                {
+                    bs.reset();
+                    j = i;
+                    break outer;
+                }
+            }
+        }
+        bs.read(array, 0, j);
+    }
     /** Imports measurement file from an InputStream.
      *
      * @param s the stream to import the measurement from
@@ -353,6 +375,7 @@ public class PANImport {
         BufferedInputStream bs = new BufferedInputStream(s);
         Data data;
         byte[] header = new byte[10];
+        int ch;
         bs.mark(16);
         bs.read(header, 0, 10);
         bs.reset();
@@ -360,13 +383,19 @@ public class PANImport {
         {
             return X00Import(bs);
         }
-        bs.mark(16*1024*1024);
-        data = XRDMLImport(bs);
-        if (data == null)
+        readWhiteSpace(bs);
+        bs.mark(16);
+        ch = bs.read();
+        bs.reset();
+        if (ch == '<')
         {
-            bs.reset();
-            return asciiImport(bs);
+            data = XRDMLImport(bs);
+            if (data == null)
+            {
+                throw new ImportException();
+            }
+            return data;
         }
-        return data;
+        return asciiImport(bs);
     }
 };
