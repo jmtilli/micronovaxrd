20365e07ed862013777208dc00a5c7c102c132fc
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2017-07-10 18:09:49 +0300

    Add support for Bruker's RAW format

--- javasrc/fi/micronova/tkk/xray/measimport/PANImport.java
+++ javasrc/fi/micronova/tkk/xray/measimport/PANImport.java
@@ -366,6 +366,204 @@ outer:
         }
         bs.read(array, 0, j);
     }
+    public static short readLe16(InputStream s) throws IOException
+    {
+        byte[] header = new byte[2];
+        if (s.read(header, 0, 2) != 2)
+        {
+            throw new IOException("EOF");
+        }
+        return (short)(((short)header[0]&0xFF) | (((short)header[1]&0xFF)<<8));
+    }
+    public static int readLe32(InputStream s) throws IOException
+    {
+        byte[] header = new byte[4];
+        if (s.read(header, 0, 4) != 4)
+        {
+            throw new IOException("EOF");
+        }
+        return (header[0]&0xFF) | (((int)header[1]&0xFF)<<8) |
+               (((int)header[2]&0xFF)<<16) | (((int)header[3]&0xFF)<<24);
+    }
+    public static long readLe64(InputStream s) throws IOException
+    {
+        byte[] header = new byte[8];
+        if (s.read(header, 0, 8) != 8)
+        {
+            throw new IOException("EOF");
+        }
+        return (header[0]&0xFF) | (((long)header[1]&0xFF)<<8) |
+               (((long)header[2]&0xFF)<<16) | (((long)header[3]&0xFF)<<24) |
+               (((long)header[4]&0xFF)<<32) | (((long)header[5]&0xFF)<<40) |
+               (((long)header[6]&0xFF)<<48) | (((long)header[7]&0xFF)<<56);
+    }
+    public static float readLeFloat(InputStream s) throws IOException
+    {
+        int bits = readLe32(s);
+        return Float.intBitsToFloat(bits);
+    }
+    public static double readLeDouble(InputStream s) throws IOException
+    {
+        long bits = readLe64(s);
+        return Double.longBitsToDouble(bits);
+    }
+    public static Data brukerImport1(InputStream s)
+        throws ImportException, IOException
+    {
+        byte[] buf = new byte[1024];
+        double[] alpha_0, meas;
+        if (s.read(buf, 0, 4) != 4)
+        {
+            throw new ImportException();
+        }
+        if (buf[0] != 'R' || buf[1] != 'A' || buf[2] != 'W' || buf[3] != ' ')
+        {
+            throw new ImportException();
+        }
+        int steps = readLe32(s);
+        alpha_0 = new double[steps];
+        meas = new double[steps];
+        float timePerStep = readLeFloat(s);
+        float xStep = readLeFloat(s);
+        s.skip(8);
+        float xStart = readLeFloat(s);
+        float thetaStart = readLeFloat(s);
+        if (thetaStart == -1e6)
+        {
+            throw new ImportException();
+        }
+        s.skip(4);
+        s.skip(4);
+        s.skip(32);
+        s.skip(4);
+        s.skip(4);
+        s.skip(72);
+        if (readLe32(s) != 0)
+        {
+            throw new ImportException();
+        }
+        for (int i = 0; i < steps; i++)
+        {
+            alpha_0[i] = (xStart + xStep*i)/2.0;
+            meas[i] = readLeFloat(s);
+        }
+        return new Data(new double[][]{alpha_0, meas});
+    }
+        
+    public static Data brukerImport2(InputStream s)
+        throws ImportException, IOException
+    {
+        byte[] buf = new byte[1024];
+        double[] alpha_0, meas;
+        if (s.read(buf, 0, 4) != 4)
+        {
+            throw new ImportException();
+        }
+        if (buf[0] != 'R' || buf[1] != 'A' || buf[2] != 'W' || buf[3] != '2')
+        {
+            throw new ImportException();
+        }
+        if (readLe16(s) != 1) // range count
+        {
+            throw new ImportException();
+        }
+        s.skip(162);
+        s.skip(22);
+        s.skip(5*4);
+        s.skip(4);
+        s.skip(42);
+        short curHeaderLen = readLe16(s);
+        if (curHeaderLen < 48)
+        {
+            throw new ImportException();
+        }
+        short steps = readLe16(s);
+        alpha_0 = new double[steps];
+        meas = new double[steps];
+        s.skip(4);
+        float timePerStep = readLeFloat(s);
+        float xStep = readLeFloat(s);
+        float xStart = readLeFloat(s);
+        s.skip(28);
+        s.skip(curHeaderLen - 48);
+        
+        for (int i = 0; i < steps; i++)
+        {
+            alpha_0[i] = (xStart + xStep*i)/2.0;
+            meas[i] = readLeFloat(s);
+        }
+        return new Data(new double[][]{alpha_0, meas});
+    }
+        
+    public static Data brukerImport101(InputStream s)
+        throws ImportException, IOException
+    {
+        byte[] buf = new byte[1024];
+        double[] alpha_0, meas;
+        if (s.read(buf, 0, 8) != 8)
+        {
+            throw new ImportException();
+        }
+        if (buf[0] != 'R' || buf[1] != 'A' || buf[2] != 'W' || buf[3] != '1' ||
+            buf[4] != '.' || buf[5] != '0' || buf[6] != '1' || buf[7] != 0)
+        {
+            throw new ImportException();
+        }
+        if (readLe32(s) != 1) // 1 means successful
+        {
+            throw new ImportException();
+        }
+        if (readLe32(s) != 1) // 1 means range count is 1
+        {
+            throw new ImportException();
+        }
+        s.skip(10 + 10 + 72 + 218 + 60 + 160);
+        s.skip(2 + 4*15);
+        s.skip(4); // anode material
+        s.skip(5*8); // alpha/beta doubles for radiation
+        s.skip(4*3);
+        s.skip(43 + 1);
+        s.skip(4);
+        // range header begins here
+        if (readLe32(s) != 304) // header len
+        {
+            throw new ImportException();
+        }
+        int steps = readLe32(s);
+        alpha_0 = new double[steps];
+        meas = new double[steps];
+        double startTheta = readLeDouble(s);
+        double startTwoTheta = readLeDouble(s);
+        s.skip(6*8);
+        s.skip(6+2);
+        s.skip(8);
+        s.skip(6+2);
+        s.skip(4);
+        s.skip(4*5);
+        s.skip(8);
+        s.skip(4*2);
+        s.skip(5+3);
+        s.skip(8*3);
+        s.skip(4*2);
+        double stepSize = readLeDouble(s);
+        s.skip(8);
+        float timePerStep = readLeFloat(s);
+        s.skip(11*4);
+        s.skip(8);
+        s.skip(4*2);
+        int supplementaryHeaderSize = readLe32(s);
+        s.skip(4*3);
+        s.skip(8+24);
+        s.skip(supplementaryHeaderSize);
+        for (int i = 0; i < steps; i++)
+        {
+            // XXX which one of these is correct?
+            //alpha_0[i] = (startTwoTheta + 2*stepSize*i)/2.0;
+            alpha_0[i] = (startTwoTheta + stepSize*i)/2.0;
+            meas[i] = readLeFloat(s);
+        }
+        return new Data(new double[][]{alpha_0, meas});
+    }
     /** Imports measurement file from an InputStream.
      *
      * @param s the stream to import the measurement from
@@ -399,6 +597,21 @@ outer:
             bs.read(header, 0, 10);
             bs.reset();
         }
+        if (header[0] == 'R' && header[1] == 'A' &&
+            header[2] == 'W' && header[3] == '1')
+        {
+            return brukerImport101(bs);
+        }
+        if (header[0] == 'R' && header[1] == 'A' &&
+            header[2] == 'W' && header[3] == '2')
+        {
+            return brukerImport2(bs);
+        }
+        if (header[0] == 'R' && header[1] == 'A' &&
+            header[2] == 'W' && header[3] == ' ')
+        {
+            return brukerImport1(bs);
+        }
         if (new String(header).equals("HR-XRDScan"))
         {
             return X00Import(bs);
@@ -417,5 +630,17 @@ outer:
             return data;
         }
         return asciiImport(bs);
+    }
+    public static void main(String[] args) throws Throwable
+    {
+        Data dat = PANImport(new FileInputStream(args[0]));
+        for (int i = 0; i < dat.arrays[0].length; i++)
+        {
+            for (int j = 0; j < dat.arrays.length; j++)
+            {
+                System.out.print(dat.arrays[j][i] + " ");
+            }
+            System.out.println();
+        }
     }
 };
