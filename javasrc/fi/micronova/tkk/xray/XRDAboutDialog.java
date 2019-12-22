5d18577a9e7a2fed2086ee7c15289732d206cb5d
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2019-12-22 15:11:03 +0200

    License for release

--- javasrc/fi/micronova/tkk/xray/XRDAboutDialog.java
+++ javasrc/fi/micronova/tkk/xray/XRDAboutDialog.java
@@ -15,49 +15,31 @@ public class XRDAboutDialog extends GenericAboutDialog {
     private static final String title = "About XRD";
     private static final String text =
 
-"Micronova XRD fitting software, version 0.1\n"+
+"Micronova XRD fitting software, version 1.0\n"+
 "\n"+
-"IMPORTANT NOTICE: This is a beta version which is NOT released\n"+
-"under the following license:\n"+
+"Copyright 2006-2014 Aalto University\n"+
+"Copyright 2006-2019 Juha-Matti Tilli\n"+
 "\n"+
-"Copyright (c) 2007 Micronova, Helsinki University of Technology\n"+
-"All rights reserved.\n"+
-"\n"+
-"Author:\n"+
+"Authors:\n"+
 "  Juha-Matti Tilli <juha-matti.tilli@iki.fi>\n"+
 "\n"+
-"Redistribution and use in source and binary forms, with or without\n"+
-"modification, are permitted provided that the following conditions\n"+
-"are met:\n"+
-"1. Redistributions of source code must retain the above copyright\n"+
-"   notice, the above list of authors, this list of conditions and\n"+
-"   the following disclaimer.\n"+
-"2. Redistributions in binary form must reproduce the above copyright\n"+
-"   notice, the above list of authors, this list of conditions and\n"+
-"   the following disclaimer in the documentation and/or other materials\n"+
-"   provided with the distribution.\n"+
-"3. All advertising materials mentioning features or use of this software\n"+
-"   must display the following acknowledgement:\n"+
-"     This product includes software developed by Micronova, Helsinki\n"+
-"     University of Technology\n"+
-/*
-"4. All scientific articles making use of this software must cite the\n"+
-"   following article:\n"+
-"     Tiilikainen J, Tilli J-M, Bosund V, Mattila M, Hakkarainen T,\n"+
-"     Airaksinen V-M and Lipsanen H 2007 J. Phys D: Appl. Phys. 40 215-8\n"+
-*/
+"Permission is hereby granted, free of charge, to any person obtaining a copy of\n"+
+"this software and associated documentation files (the \"Software\"), to deal in \n"+
+"the Software without restriction, including without limitation the rights to\n"+
+"use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies\n"+
+"of the Software, and to permit persons to whom the Software is furnished to do\n"+
+"so, subject to the following conditions:\n"+
 "\n"+
-"THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS\n"+
-"``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED\n"+
-"TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR\n"+
-"PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS\n"+
-"BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR\n"+
-"CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF\n"+
-"SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS\n"+
-"INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN\n"+
-"CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)\n"+
-"ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE\n"+
-"POSSIBILITY OF SUCH DAMAGE.\n"+
+"The above copyright notice and this permission notice shall be included in all \n"+
+"copies or substantial portions of the Software.\n"+
+"\n"+
+"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR \n"+
+"IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"+
+"FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"+
+"AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"+
+"LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n"+
+"OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n"+
+"SOFTWARE.\n"+
 "\n"+
 "This program uses XChart, Copyright 2011-2015 Xeiam LLC (http://xeiam.com)\n"+
 "and contributors, and Copyright 2015-2017 Knowm Inc. (http://knowm.org) and\n"+
